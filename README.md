c61c65d29cf61b754088ff2cb8376b9dbd4472dd
nandanpandey87 <46093719+nandanpandey87@users.noreply.github.com>
2021-03-21 22:18:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Donation_Portal
+  testing donation portal 
---
+++ home.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Donation Portal</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css ">		
+	</head>
+	<body class="landing">
+
+			<header id="header" class="alt">
+				<h1 ><a href="home.php" >Donation Portal - </a>   Sparks Foundation</h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="home.php">Home</a></li>
+						<li><a href="#one">About</a></li>
+						<li><a fforhref="#two">Contact</a></li>
+						<li><a href="index.php">Logout</a></li>
+					</ul>
+				</nav>
+			</header>
+
+			<a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+
+			<section id="banner">
+				<h2>Welcome To Sparks Donation Portal!</h2>
+				<p>We are Giving things for happiness <br /> Do a good deed, give to those in need.</p>
+			
+				<ul class="actions">
+				<form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_GpNBhF7v0lGIZV" async> </script> </form>
+				</ul>
+			</section>
+
+			<!-- One -->
+				<section id="one" class="wrapper style1">
+					<div class="container 75%">
+						<div class="row 200%">
+							<div class="6u 12u$(medium)">
+								<header class="major">
+									<h2>Sparks Donation</h2>
+									<p>Every Good Act Counts</p>
+								</header>
+							</div>
+							<div class="6u$ 12u$(medium)">
+								<p style="font-weight: bold; font-style: italic; ">We are working  with an objective to help people in need as the true value of one's life can be measured if the person is able to make a significant difference in other's Life.</p>
+								<p style="font-weight: bold; font-style: italic;">Please Note : This is just a demo portal build as an intern working for Sparks Foundation. The money you enter will just be digits and will not be paid to anyone and this payment gateway is just for testing purpose. </p>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- two -->
+				<section id="two" class="wrapper style3 special">
+					<div class="container">
+						<header class="major">
+							<h2>Sparks Foundation Internship</h2>
+							<p style="font:#FFFFFF;font-weight: bold;"> Nandan Pandey </p>
+						</header>
+						<ul class="actions">
+							<li><a href="https://www.linkedin.com/in/nandan-p-7b8215126/" class="button special big">Get in touch</a></li>
+						</ul>
+					</div>
+				</section>
+
+		<!-- Logos -->
+			<footer id="footer">
+				<div class="container">
+					<ul class="icons">
+						<li><a href="https://github.com/nandanpandey87" class="fa fa-github"></a></li>
+						<li><a href="https://www.linkedin.com/in/nandan-p-7b8215126/" class="fa fa-linkedin"></a></li>
+						
+					</ul>
+					<ul class="copyright">
+						<li style="font-weight: bold;">&copy; No Copyright Issue,Just Ping Me Anytime Nandan Pandey</li>
+					</ul>
+				</div>
+			</footer>
+
+	</body>
+</html>
